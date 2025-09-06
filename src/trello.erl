9ab66322a1c1a784d3a410d99ca29adab50391a9
Peer Stritzinger <peer@stritzinger.com>
2025-09-06 16:08:51 +0200

    feat(trello): add update_card/2; CT for updating name/desc; HTTP PUT helper

--- src/trello.erl
+++ src/trello.erl
@@ -1,7 +1,10 @@
 -module(trello).
 
 -export([
-    me/0
+    me/0,
+    get_card/1,
+    create_card/1,
+    update_card/2
 ]).
 
 -define(BASE_URL, "https://api.trello.com/1").
@@ -22,6 +25,59 @@ do_get(Path, QueryKVs) ->
             Url = build_url(Path, Query),
             http_get_json(Url)
     end.
+
+do_post(Path, QueryKVs) ->
+    case {application:get_env(trellang, trello_key), application:get_env(trellang, trello_token)} of
+        {undefined, _} -> {error, missing_trello_key};
+        {_, undefined} -> {error, missing_trello_token};
+        {{ok, Key0}, {ok, Token0}} ->
+            Key = to_bin(Key0),
+            Token = to_bin(Token0),
+            Query = [{<<"key">>, Key}, {<<"token">>, Token} | QueryKVs],
+            Url = build_url(Path, Query),
+            http_post_json(Url)
+    end.
+
+get_card(CardId0) ->
+    CardId = to_bin(CardId0),
+    Fields = <<"id,name,idBoard">>,
+    case do_get(["/cards/", CardId], [{<<"fields">>, Fields}]) of
+        {ok, Map} ->
+            case application:get_env(trellang, board_id) of
+                undefined -> {error, missing_board_id};
+                {ok, BoardId0} ->
+                    BoardId = to_bin(BoardId0),
+                    case maps:get(<<"idBoard">>, Map, undefined) of
+                        BoardId -> {ok, Map};
+                        _Other -> {error, forbidden_board}
+                    end
+            end;
+        Other -> Other
+    end.
+
+create_card(Options) when is_map(Options) ->
+    case {application:get_env(trellang, list_id)} of
+        {undefined} -> {error, missing_list_id};
+        {{ok, ListId0}} ->
+            ListId = to_bin(ListId0),
+            NameBin = to_bin(maps:get(name, Options, <<>>)),
+            Query = [
+                {<<"idList">>, ListId},
+                {<<"name">>, NameBin}
+            ],
+            do_post("/cards", Query)
+    end.
+
+update_card(CardId0, Updates) when is_map(Updates) ->
+    CardId = to_bin(CardId0),
+    Query = maps:fold(fun
+        (name, V, Acc) -> [{<<"name">>, to_bin(V)} | Acc];
+        (desc, V, Acc) -> [{<<"desc">>, to_bin(V)} | Acc];
+        (due, V, Acc)  -> [{<<"due">>, to_bin(V)} | Acc];
+        (pos, V, Acc)  -> [{<<"pos">>, to_bin(V)} | Acc];
+        (_K, _V, Acc) -> Acc
+    end, [], Updates),
+    do_put(["/cards/", CardId], Query).
 
 build_url(Path, QueryKVs) ->
     Qs = uri_string:compose_query(QueryKVs),
@@ -53,6 +109,74 @@ http_get_json_sni(Url, SNIHost) ->
             {error, {http_error, Code, Body}};
         {error, Reason} ->
             {error, Reason}
+    end.
+
+http_post_json(Url) ->
+    case http_post_json_sni(Url, "api.trello.com") of
+        {error, {failed_connect, _}} ->
+            case http_post_json_sni(Url, "trello.com") of
+                {error, _}=E2 -> E2;
+                Ok -> Ok
+            end;
+        Other -> Other
+    end.
+
+http_post_json_sni(Url, SNIHost) ->
+    SSLOpts = [
+        {server_name_indication, SNIHost},
+        {verify, verify_peer},
+        {cacerts, public_key:cacerts_get()}
+    ],
+    HTTPOpts = [{ssl, SSLOpts}],
+    Headers = [],
+    Body = <<>>,
+    ReqOpts = [{body_format, binary}],
+    case httpc:request(post, {Url, Headers, "application/x-www-form-urlencoded", Body}, HTTPOpts, ReqOpts) of
+        {ok, {{_Vsn, Code, _}, _Headers, RespBody}} when Code =:= 200; Code =:= 201 ->
+            {ok, json:decode(RespBody)};
+        {ok, {{_Vsn, Code2, _}, _Headers2, RespBody2}} ->
+            {error, {http_error, Code2, RespBody2}};
+        {error, Reason} -> {error, Reason}
+    end.
+
+do_put(Path, QueryKVs) ->
+    case {application:get_env(trellang, trello_key), application:get_env(trellang, trello_token)} of
+        {undefined, _} -> {error, missing_trello_key};
+        {_, undefined} -> {error, missing_trello_token};
+        {{ok, Key0}, {ok, Token0}} ->
+            Key = to_bin(Key0),
+            Token = to_bin(Token0),
+            Query = [{<<"key">>, Key}, {<<"token">>, Token} | QueryKVs],
+            Url = build_url(Path, Query),
+            http_put_json(Url)
+    end.
+
+http_put_json(Url) ->
+    case http_put_json_sni(Url, "api.trello.com") of
+        {error, {failed_connect, _}} ->
+            case http_put_json_sni(Url, "trello.com") of
+                {error, _}=E2 -> E2;
+                Ok -> Ok
+            end;
+        Other -> Other
+    end.
+
+http_put_json_sni(Url, SNIHost) ->
+    SSLOpts = [
+        {server_name_indication, SNIHost},
+        {verify, verify_peer},
+        {cacerts, public_key:cacerts_get()}
+    ],
+    HTTPOpts = [{ssl, SSLOpts}],
+    Headers = [],
+    Body = <<>>,
+    ReqOpts = [{body_format, binary}],
+    case httpc:request(put, {Url, Headers, "application/x-www-form-urlencoded", Body}, HTTPOpts, ReqOpts) of
+        {ok, {{_Vsn, Code, _}, _Headers, RespBody}} when Code =:= 200; Code =:= 201 ->
+            {ok, json:decode(RespBody)};
+        {ok, {{_Vsn, Code2, _}, _Headers2, RespBody2}} ->
+            {error, {http_error, Code2, RespBody2}};
+        {error, Reason} -> {error, Reason}
     end.
 
 to_bin(B) when is_binary(B) -> B;
---
+++ test/trello_cards_update_SUITE.erl
@@ -0,0 +1,50 @@
+-module(trello_cards_update_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+
+-export([suite/0, all/0, init_per_suite/1, end_per_suite/1]).
+-export([update_name_and_desc/1]).
+
+suite() -> [{timetrap, {seconds, 90}}].
+all() -> [update_name_and_desc].
+
+init_per_suite(Config) ->
+    ok = ensure_started(ssl),
+    ok = ensure_started(inets),
+    ok = ensure_loaded(trellang),
+    Config.
+
+end_per_suite(_Config) -> ok.
+
+update_name_and_desc(_Config) ->
+    case {application:get_env(trellang, board_id), application:get_env(trellang, list_id)} of
+        {undefined, _} -> {skip, "missing trellang.board_id in dev.config"};
+        {_, undefined} -> {skip, "missing trellang.list_id in dev.config"};
+        {_, _} ->
+            {ok, Card0} = trello:create_card(#{name => <<"CT update test">>}),
+            CardId = maps:get(<<"id">>, Card0),
+            {ok, Updated} = trello:update_card(CardId, #{name => <<"Updated name">>, desc => <<"Updated desc">>}),
+            %% verify echoed fields
+            <<"Updated name">> = maps:get(<<"name">>, Updated),
+            <<"Updated desc">> = maps:get(<<"desc">>, Updated),
+            %% fetch again to be sure persisted
+            {ok, Card} = trello:get_card(CardId),
+            <<"Updated name">> = maps:get(<<"name">>, Card),
+            ok
+    end.
+
+ensure_started(App) ->
+    case application:ensure_all_started(App) of
+        {ok, _} -> ok;
+        {error, {already_started, _}} -> ok;
+        {error, Reason} -> error(Reason)
+    end.
+
+ensure_loaded(App) ->
+    case application:load(App) of
+        ok -> ok;
+        {error, {already_loaded, App}} -> ok;
+        {error, Reason} -> error(Reason)
+    end.
+
+
